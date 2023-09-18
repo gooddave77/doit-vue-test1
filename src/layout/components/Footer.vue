161f74f701987c2de3b988304626504b1cf37470
david park <gooddave77@gmail.com>
2023-09-18 12:33:53 +0900

    1st-commit

---
+++ src/layout/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer>
+        <div class="botinfo"></div>
+            <div class="botbtngroup">
+                <button class="btn-text">찾아오시는 길</button>
+                <button class="btn-text">개인정보취급방침</button>
+                <button class="btn-text">이용 약관</button>
+                <button class="btn-gray">저자 신청</button>
+                <button class="btn-gray">역자 신청</button>
+                <button class="btn-gray">교재샘플/강의자료 신청</button>
+            </div>
+        <div class="footerinfo">
+            <div class="footer-logo"><img src="/images/footer-logo.png" /></div>
+            <div class="footer-sub">
+                <address>
+                    서울시 마포구 이지스퍼블리싱 <span>Tel 02-1234-5678</span>
+                </address>
+                <p>Email : study-vue@gmail.com</p>
+                <p>통신판매업신고 : 2023-gangnam-01234</p>
+            </div>
+        </div>
+    </footer>
+</template>
---
+++ src/layout/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <section id="wrap">
+        <h1 class="bind">웹싸이트제목:VUE스터디</h1>
+        <Header />
+        <section>상단 영역</section>
+        <section id="container">
+            <router-view />
+        </section>
+        <Footer />
+        <section>하단 영역</section>
+    </section>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+export default {
+    components: {
+        Header,
+        Footer,
+    },
+};
+</script>
